887fe110d7871eaf5ce3607b7ad7217aafc19548
Miroslav <kolosnjaji@stcable.net>
2024-07-11 15:33:18 +0200

    Implemented PurchaseOrderItemIdTest class

---
+++ src/test/java/com/myproject/autopartsestoresystem/model/PurchaseOrderItemIdTest.java
@@ -0,0 +1,38 @@
+package com.myproject.autopartsestoresystem.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Miroslav Kološnjaji
+ */
+class PurchaseOrderItemIdTest {
+
+    @Test
+    void testAllFields_isEqual_thenCorrect() {
+
+        //given
+        PurchaseOrderItemId purchaseOrderItemId1 = PurchaseOrderItemId.builder()
+                .purchaseOrderId(1L)
+                .ordinalNumber(1)
+                .build();
+
+        PurchaseOrderItemId purchaseOrderItemId2 = PurchaseOrderItemId.builder()
+                .purchaseOrderId(1L)
+                .ordinalNumber(1)
+                .build();
+
+        boolean isequal = purchaseOrderItemId1.equals(purchaseOrderItemId2);
+        boolean hasCode = purchaseOrderItemId1.hashCode() == purchaseOrderItemId2.hashCode();
+
+        //when
+        assertAll("PurchaseOrderItemID fields validation",
+                () -> assertEquals(purchaseOrderItemId1.getPurchaseOrderId(), purchaseOrderItemId2.getPurchaseOrderId(), "Purchase Order Item ID does not equal"),
+                () -> assertEquals(purchaseOrderItemId1.getOrdinalNumber(), purchaseOrderItemId2.getOrdinalNumber(), "Purchase Order Item ordinal number does not equal"));
+
+        assertTrue(isequal, "Purchase Order Item ID object are not equal");
+        assertTrue(hasCode, "Purchase Order Item ID hashCode are not equal");
+
+    }
+}

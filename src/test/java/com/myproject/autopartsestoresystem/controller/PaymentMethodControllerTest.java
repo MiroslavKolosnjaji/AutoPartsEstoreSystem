cc9cd6885b3c65a16550e8d98b78a6883eb54fe3
Miroslav <kolosnjaji@stcable.net>
2024-07-13 19:55:37 +0200

    Added additional tests to PaymentMethodControllerTest to cover edge cases.

--- src/test/java/com/myproject/autopartsestoresystem/controller/PaymentMethodControllerTest.java
+++ src/test/java/com/myproject/autopartsestoresystem/controller/PaymentMethodControllerTest.java
@@ -2,10 +2,11 @@ package com.myproject.autopartsestoresystem.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myproject.autopartsestoresystem.dto.PaymentMethodDTO;
-import com.myproject.autopartsestoresystem.model.PaymentMethod;
+import com.myproject.autopartsestoresystem.exception.service.PaymentMethodNotFoundException;
 import com.myproject.autopartsestoresystem.model.PaymentType;
 import com.myproject.autopartsestoresystem.service.PaymentMethodService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,7 +17,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -46,6 +46,7 @@ class PaymentMethodControllerTest {
         paymentMethodDTO = PaymentMethodDTO.builder().id(1L).paymentType(PaymentType.DEBIT_CARD).build();
     }
 
+    @DisplayName("Get All Payment Methods")
     @Test
     void testGetAllPaymentMethods_whenListIsPopulated_returns200StatusCode() throws Exception {
 
@@ -67,6 +68,28 @@ class PaymentMethodControllerTest {
         verify(paymentMethodService).getAll();
     }
 
+    @DisplayName("Get All Payment Methods - List Is Empty")
+    @Test
+    void testGetAllPaymentMethods_whenListIsEmpty_returns404StatusCode() throws Exception {
+
+        //given
+        List<PaymentMethodDTO> emptyList = List.of();
+
+        when(paymentMethodService.getAll()).thenReturn(emptyList);
+
+        RequestBuilder requestBuilder = get(PaymentMethodController.PAYMENT_METHOD_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+
+        //when
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        //then
+        assertEquals(HttpStatus.NOT_FOUND.value(), result.getResponse().getStatus(), "Status not match. Expected 404 status code");
+        verify(paymentMethodService).getAll();
+    }
+
+    @DisplayName("Get Payment Method By ID")
     @Test
     void testGetPaymentMethodById_whenValidIdProvided_returns200StatusCode() throws Exception {
 
@@ -93,6 +116,24 @@ class PaymentMethodControllerTest {
 
     }
 
+    @DisplayName("Get Payment Method By ID Failed - Invalid ID Provided")
+    @Test
+    void testGetPaymentMethodByID_whenInvalidMethodProvided_returns404StatusCode() throws Exception {
+
+        //given
+        when(paymentMethodService.getById(anyLong())).thenThrow(PaymentMethodNotFoundException.class);
+
+        RequestBuilder requestBuilder = get(PaymentMethodController.PAYMENT_METHOD_URI_WITH_ID, 1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        assertEquals(HttpStatus.NOT_FOUND.value(), result.getResponse().getStatus(), "Status mismatch. Expected Status code 404");
+        verify(paymentMethodService).getById(anyLong());
+    }
+
+    @DisplayName("Get Payment Method By Payment Type")
     @Test
     void testGetPaymentMethodByPaymentType_whenValidPaymentTypeProvided_returns200StatusCode() throws Exception {
 
@@ -113,6 +154,25 @@ class PaymentMethodControllerTest {
         assertEquals(foundDTO.getId(), paymentMethodDTO.getId(), "Payment method id mismatch");
         assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus(), "Status mismatch. Expected Status code 200");
 
+        verify(paymentMethodService).getByPaymentType(anyString());
+    }
+
+    @Test
+    void testGetPaymentMethodByPaymentType_whenInvalidPaymentTypeProvided_returns404StatusCode() throws Exception {
+
+        //given
+        when(paymentMethodService.getByPaymentType(anyString())).thenThrow(PaymentMethodNotFoundException.class);
+
+        RequestBuilder requestBuilder = get(PaymentMethodController.PAYMENT_METHOD_URI + "/payment_type", anyString())
+                .param("payment_type", PaymentType.CREDIT_CARD.name())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+
+        //when
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        //then
+        assertEquals(HttpStatus.NOT_FOUND.value(), result.getResponse().getStatus(), "Status mismatch. Expected Status code 404");
         verify(paymentMethodService).getByPaymentType(anyString());
     }
 }

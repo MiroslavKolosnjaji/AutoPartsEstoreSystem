145a5b72bd83f1f1390aa4d31e0e09d69fc60392
Miroslav <kolosnjaji@stcable.net>
2024-08-02 20:34:59 +0200

    Added throws clauses to CRUD methods in the CRUDService interface

--- src/main/java/com/myproject/autopartsestoresystem/service/CrudService.java
+++ src/main/java/com/myproject/autopartsestoresystem/service/CrudService.java
@@ -1,5 +1,8 @@
 package com.myproject.autopartsestoresystem.service;
 
+import com.myproject.autopartsestoresystem.exception.controller.EntityAlreadyExistsException;
+import com.myproject.autopartsestoresystem.exception.controller.EntityNotFoundException;
+
 import java.util.List;
 
 /**
@@ -7,9 +10,9 @@ import java.util.List;
  */
 public interface CrudService<T, ID> {
 
-    T save(T entity);
-    T update(ID id, T entity);
+    T save(T entity) throws EntityAlreadyExistsException;
+    T update(ID id, T entity) throws EntityNotFoundException;
     List<T> getAll();
-    T getById(ID id);
-    void delete(ID id);
+    T getById(ID id) throws EntityNotFoundException;
+    void delete(ID id) throws EntityNotFoundException;
 }

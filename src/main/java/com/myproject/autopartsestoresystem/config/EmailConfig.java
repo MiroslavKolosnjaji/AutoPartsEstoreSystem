aa6f9928e891b7b8f9b5bac6195712cbfa40c5a4
Miroslav <kolosnjaji@stcable.net>
2024-08-29 18:14:10 +0200

    Updated application.properties with email parameters and updated 'EmailConfig' class to read that values

--- src/main/java/com/myproject/autopartsestoresystem/config/EmailConfig.java
+++ src/main/java/com/myproject/autopartsestoresystem/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.myproject.autopartsestoresystem.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,12 +14,25 @@ import java.util.Properties;
 @Configuration
 public class EmailConfig {
 
+    @Value("${spring.mail.host}")
     private String host;
+
+    @Value("${spring.mail.port}")
     private int port;
+
+    @Value("${spring.mail.username}")
     private String username;
+
+    @Value("${spring.mail.password}")
     private String password;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
     private String smtpAuth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
     private String starttlsEnable;
+
+    @Value("${spring.mail.default-encoding}")
     private String defaultEncoding;
 
     @Bean
